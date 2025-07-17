185e1b3c2a4f4aa61b0f09067e4f3c7abbf9389a
shushanth2003 <bsshu2003@gmail.com>
2025-07-17 09:02:29 +0530

    Feat: adding variable code in it

---
+++ Function-Scope/datavariable.js
@@ -0,0 +1,13 @@
+//variables
+
+var a="shushanth";
+console.log(a);
+
+//let variable we can reassign but it is block scope
+
+let b="ramkumar";
+console.log(b);
+
+//const variable we cannot reassign so its block scope
+const c="devakumar"
+console.log(c);
