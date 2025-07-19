669984d9ba0649b8fb07767c77e2af98b5005f34
shushanth2003 <bsshu2003@gmail.com>
2025-07-19 20:31:19 +0530

    Initial Command prompt only

--- Block-Shadowing-Scope/blockScope.js
+++ Block-Shadowing-Scope/blockScope.js
@@ -4,3 +4,10 @@ let a="react";
     console.log("Inside block : ",a);
 }
 console.log("Outside block : ",a);
+
+let b="markUpLanguage";
+{
+    let b="levelUpLanguage";
+    console.log("Outside block of the b : ",b);
+}
+console.log(b);
